368caed6cc52f64d7d3fb655d7368af8c62200cc
Jonathan <jbonzy@bestofmedia.com>
2011-06-22 14:58:45 +0200

    add get method to retrieve content form an url

--- lib/Curl.php
+++ lib/Curl.php
@@ -1,7 +1,60 @@
 <?php
 
 class Curl {
- 
+
+  private $curlHandler;
+  public static $CURL_OPTS = array(
+      CURLOPT_CONNECTTIMEOUT => 10,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_TIMEOUT => 60
+  );
+
+  public function __construct() {
+    
+  }
+
+  public function init() {
+    if (!function_exists('curl_init')) {
+      throw new Exception('Application needs the CURL PHP extension');
+    }
+    $this->curlHandler = curl_init();
+  }
   
+  protected function setUrlAndOptions($url) {
+    $options = self::$CURL_OPTS;
+    $options[CURLOPT_URL] = $url;
+    curl_setopt_array($this->curlHandler, $options);
+  }
+  
+  protected function getInfo() {
+    return curl_getinfo($this->curlHandler);
+  }
+  
+  protected function close() {
+     curl_close($this->curlHandler);
+  }
+  
+  protected function retrieveErrors(&$header) {
+    $header['error_number']   = curl_errno($this->curlHandler);
+    $header['error_message']  = curl_error($this->curlHandler);
+  }
+  
+  protected function setResponseContent(&$header, $content) {
+    $header['content'] = $content;
+  }
+  
+  protected function executeRequest() {
+    return curl_exec($this->curlHandler);
+  }
+  
+  public function get($url) {
+    $this->setUrlAndOptions($url);
+    $content = $this->executeRequest();
+    $header = $this->getInfo();
+    $this->retrieveErrors($header);
+    $this->setResponseContent($header, $content);
+    $this->close();
+    return $header;
+  }
 }
 
--- test/unit/lib/CurlTest.php
+++ test/unit/lib/CurlTest.php
@@ -6,6 +6,12 @@ class CurlTest extends PHPUnit_Framework_TestCase {
 
   public function testItGetsAnUrlAndRetrievesData() {
     $curl = new Curl();
+    $curl->init();
+    $result = $curl->get('http://www.google.fr');
+    $this->assertNotEmpty($result);
+    $this->assertEquals($result['http_code'], 200);
+    $this->assertEquals($result['error_number'], 0);
+    $this->assertEquals($result['error_message'], '');
   }
 
 }
